ab41eb0e0dc8d8323397f663e4521b1d60ae8dce
unknown <qabelisaias@gmail.com>
2024-10-29 12:00:57 -0500

    feat:flask server

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+#Crear la Aplicación
 app = Flask(__name__)
 
+#Crear la Ruta Principal
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+#Ejecutar la Aplicación
 if __name__ == '__main__':
     app.run(debug=True)
