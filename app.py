f67e14d14ee01e7fd9049e62e5db277acae243ce
unknown <qabelisaias@gmail.com>
2024-11-04 22:23:44 -0500

    feat: adding database and endpoint to fetchall posts

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("midatabase.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 #Crear la Aplicación
 app = Flask(__name__)
 
-#Crear la Ruta Principal
+#Crear la Ruta Principal -> "CONTROLADOR"-> va tener la función con conectar con la base de datos
 @app.route("/", methods=["GET"])
 def base():
     context = {
@@ -19,6 +25,21 @@ def home():
         "my_name": "Jorge"
         }
     return render_template(template_name_or_list="home.html", **context)
+
+@app.route("/post", methods=["GET"])
+def get_all_posts():
+    conn = get_db_connection()
+    posts= conn.execute("SELECT * FROM posts").fetchall() # crea un lista de todas esas filas y coloca dentro de post
+    conn.close()
+    for post in posts:
+        print("======>", post["id"])
+        print("======>", post["title"])
+        print("======>", post["content"])
+        print("======>", post["created"])
+        print("============================================")
+
+    return render_template(template_name_or_list="posts.html")
+
 #Ejecutar la Aplicación
 if __name__ == '__main__':
     app.run(debug=True) 
